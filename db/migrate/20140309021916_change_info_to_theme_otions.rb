8f25943f026057727c06c7629661832b12041bba
Michael McDevitt <mmcdevi1@gmail.com>
2014-03-14 22:57:06 -0400

    Change migration file

--- db/migrate/20140309021916_change_info_to_theme_otions.rb
+++ db/migrate/20140309021916_change_info_to_theme_otions.rb
@@ -1,5 +1,11 @@
 class ChangeInfoToThemeOtions < ActiveRecord::Migration
   def change
-  	change_column :theme_options, :template, :integer
-  end
+  	rename_column :theme_options, :template, :template_old
+  	add_column :theme_options, :template, :integer
+
+  	ThemeOption.reset_column_information
+  	ThemeOption.find_each { |c| c.update_attribute(:template, c.template_old) }
+
+  	remove_column :theme_options, :template_old
+	end
 end
