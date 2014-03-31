bb6b7181dadbff5ee1f577b31861714d5edc8286
Michael McDevitt <mmcdevi1@gmail.com>
2014-03-31 15:08:23 -0400

    change email settings

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -1,15 +1,16 @@
 class IncomingMailsController < ApplicationController
+
+	require 'mail'
   skip_before_action :verify_authenticity_token
 
   def create
-    Rails.logger.log params[:envelope][:to] # print the to field to the logs
-    Rails.logger.log params[:subject] # print the subject to the logs
-    Rails.logger.log params[:plain] # print the decoded body plain to the logs if present
-    Rails.logger.log params[:html] # print the html decoded body to the logs if present
-    Rails.logger.log params[:attachments][0] if params[:attachments] # A tempfile attachment if attachments is populated
+    message = Mail.new(params[:message])
+    Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
+    Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
 
     # Do some other stuff with the mail message
 
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
   end
+
 end
